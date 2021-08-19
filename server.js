54bfb67fa9387cdbcbec8ae629b2eedff433bf34
naorgenis <naorgenis@gmail.com>
2021-08-19 19:05:32 +0300

    added port env

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5az4l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch((error) => console.log(error));
